961e1f95eaeb62ef760236484c515c25e6e03a5c
Cristiano Silva <p534722@mail.caixa>
2018-07-19 14:10:16 -0300

    my commit

---
+++ ProjetoMVC/Controllers/ClienteController.cs
@@ -0,0 +1,82 @@
+﻿using ProjetoMVC.ClienteServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoMVC.Controllers
+{
+    public class ClienteController : Controller
+    {
+        readonly ClienteServiceClient _wcf = new ClienteServiceClient();
+
+        // GET: Cliente
+        [OutputCache(Duration = 60)]
+        public ActionResult Index()
+        {
+            List<Cliente> cliente = _wcf.FindAll().ToList();
+            return View(cliente);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Cliente cliente = _wcf.Find(id);
+            return View(cliente);
+        }
+
+        public ActionResult Create()
+        {
+            Cliente cliente = new Cliente();
+            return View(cliente);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _wcf.New(cliente);
+                return RedirectToAction("Index");
+            }
+
+            return View(cliente);
+        }
+
+        //GET: Cliente/Edit/
+        public ActionResult Edit(int id)
+        {
+            Cliente cliente = _wcf.Find(id);
+            return View(cliente);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _wcf.Update(cliente);
+                return RedirectToAction("index");
+            }
+
+            return View(cliente);
+        }
+
+        //GET: Cliente/Delete
+        public ActionResult Delete(int id)
+        {
+            Cliente cliente = _wcf.Find(id);
+            return View(cliente);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            _wcf.Delete(id);
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ ProjetoMVC/Controllers/ProdutoController.cs
@@ -0,0 +1,81 @@
+﻿using ProjetoMVC.ProdutoServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoMVC.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        readonly ProdutoServiceClient _wcf = new ProdutoServiceClient();
+
+        // GET: Produto
+        public ActionResult Index()
+        {
+            List<Produto> produtos = _wcf.FindAll().ToList();
+            return View(produtos);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Produto produto = _wcf.Find(id);
+            return View(produto);
+        }
+
+        public ActionResult Create()
+        {
+            Produto produto = new Produto();
+            return View(produto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                _wcf.New(produto);
+                return RedirectToAction("Index");
+            }
+
+            return View(produto);           
+        }
+
+        //GET: Produtos/Edit/
+        public ActionResult Edit(int id)
+        {
+            Produto produto = _wcf.Find(id);
+            return View(produto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                _wcf.Update(produto);
+                return RedirectToAction("index");
+            }
+
+            return View(produto);
+        }
+
+        //GET: Produtos/Delete
+        public ActionResult Delete(int id)
+        {
+            Produto produto = _wcf.Find(id);
+            return View(produto);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            _wcf.Delete(id);
+            return RedirectToAction("index");
+        }
+    }
+}
